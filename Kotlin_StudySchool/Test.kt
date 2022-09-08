b5e17d3cff6f464d5f37034be2e5d7d935e31855
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-09-08 15:17:29 +0900

    2022-09-08 (6교시)

---
+++ Kotlin_StudySchool/Test.kt
@@ -0,0 +1,46 @@
+package com.example.kotlin_studyschool
+
+class Test {
+}
+// var, val
+//val date1 = 10
+//var date2 = 10
+
+//문자열
+//val str1 = "hello \n world"
+//var str2 = """Hello
+//       Wolrd"""
+
+//배열
+//val detal : Array<Int> = Array(3, {0})
+
+//리스트
+var list = listOf<Int>(10, 20, 30)
+
+fun main() {
+//    println("hello kotlin")
+
+//    date1 + 20 // 오류
+//    date2 + 20 // 성공
+
+//    println("str1 : $str1")
+//    println("str2 : $str2")
+
+//    detal[0] = 10
+//    detal[1] = 20
+//    detal.set(2, 30)
+//
+//    println(
+//        """
+//            array size : ${detal.size}
+//            array data : ${detal[1]}, ${detal.get(2)}
+//        """.trimIndent()
+//    )
+
+    println(
+        """
+            list size : ${list.size}
+            list date : ${list[0]}, ${list.get(1)}, ${list.get(2)}
+        """.trimIndent()
+    )
+}

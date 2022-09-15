ca198e73ef91c9e1a99658d907f87c078a39cf40
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-09-15 15:24:22 +0900

    2022-09-15 (5~6교시)

--- Kotlin_StudySchool/Test.kt
+++ Kotlin_StudySchool/Test.kt
@@ -20,7 +20,16 @@ class Test {
 //var list = listOf<Int>(10, 20, 30)
 
 //조건문
-var date = 10
+//var date = 10
+//var data = 10
+
+
+//반복문
+//var sum : Int = 0
+//var data = arrayOf<Int>(10, 20, 30)
+var x = 0
+var sum1 = 0
+
 
 fun main() {
 
@@ -53,7 +62,65 @@ fun main() {
 //    if (date > 0) {
 //        println("date > 0")
 //    } else {
-//        println("date =< 0")
+//        println("date <= 0")
 //    }
 
+//    if (data > 10) {
+//        println("data > 10")
+//    } else if (data >0 && data <= 10) {
+//        println("data > 0 && data <= 10")
+//    } else {
+//        println("data <= 0")
+//    }
+
+//    val result = if (data > 0) {
+//        println("data > 0")
+//        true  // 변환값
+//    } else {
+//        println("data <= 0")
+//        false
+//    }
+//    println(result)
+
+//    when(data) {
+//        10 -> println("data is 10")
+//        20 -> println("data is 20")
+//        else -> {
+//            println("data is not valid data")
+//        }
+//    }
+
+//    var data : Any = 10
+//    when(data) {
+//        is String -> println("data is String")  // data가 문자열 타입이면 출력
+//        20, 30 -> println("data is 20 or 30")  // data가 20 또는 30이면 출력
+//        in 1..10 -> println("data is 1..10")  // data가 1 ~ 10이면 출력
+//        else -> println("data is not valid")
+//    }
+
+//    for (i in 1..10) {
+//        sum += 1
+//    }
+//    println(sum)
+
+//    for (i in data.indices) {
+//        print(data[i])
+//        if (i != data.size - 1) print(",")
+//    }
+
+//    while (x < 10) {
+//        sum1 += ++x
+//    }
+//    println(sum1)
+
+    var x = 0
+    for(i in 1..5){
+        for(j in i..4){
+            print(" ")
+        }
+        for(j in 1 until i * 2){
+            print("*")
+        }
+        println()
+    }
 }
